94a0d82fe5fadefa80fc9fa3f1cfee70bd58aa2b
Harriet Wang <zhw142@ucsd.edu>
2020-05-01 23:32:37 -0700

    shuffle answer options (fisher-yate), handle event and bind this

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Questino from './components/Question';
+import quizQuestions from './api/quizQuestions';
 
 class App extends Component {
 
@@ -17,6 +18,32 @@ class App extends Component {
       answersCount: {},
       result: ''
     };
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const shufledAnswerOptions = quizQuestions.map((question) => this.shuffleArray(question.answers));
+
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions:shuffledAnswerOptions[0]
+    });
+
+    //randomise array (the order of the answer options)
+    //fisher-yates algorithm
+    shuffleArray(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+      while (0 !== currentIndex) {
+        //pick a remaining element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -=1;
+        //swap with current elements
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+      return array;
+    };
   }
 
   render() {
@@ -26,7 +53,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
         </header>
         <h2>React Quiz</h2>
-        <Question content="what is your favorite food?" />
+          <Quiz
+          answer={this.state.answer}
+          answerOptions={this.state.answerOptions}
+          questionId={this.state.questionId}
+          question={this.state.question}
+          questionTotal={quizQuestions.length}
+          onAnswerSelected={this.handleAnswerSelected}
+        />
       </div>
     );
   }
